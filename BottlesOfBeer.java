57b790d11b6928120805943823768ae8b39e5438
Jonathan <lennysgarage1@gmail.com>
2016-12-06 20:23:34 -0500

    Bottles of Beer Program

---
+++ BottlesOfBeer.java
@@ -0,0 +1,22 @@
+
+class BottlesOfBeer {
+  public static void main(String [] args){
+    for(int i = 99; i > -1; i--){
+      try {
+        System.out.println(i + " Bottles of beer on the wall");
+        Thread.sleep(1500);
+        System.out.println(i + " Bottles of beer");
+        Thread.sleep(1500);
+        System.out.println("Take 1 down, pass it around");
+        Thread.sleep(1500);
+        i--;
+        System.out.println(i + " Bottles of beer on the wall");
+        Thread.sleep(1500);
+        i++;
+      }
+      catch(Exception e){
+        System.out.println(e);
+      }
+    }
+}
+}
