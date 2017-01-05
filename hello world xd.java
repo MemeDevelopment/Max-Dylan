6e85c2631b429f7266bc0ee8cf0d9ff8b117c1f4
Moosiq <musiqplaysmc@gmail.com>
2017-01-04 21:01:41 -0500

    hello world or something
    lenny wanted me to say "my first hello world program added to github"

---
+++ hello world xd.java
@@ -0,0 +1,11 @@
+/**
+ * Created by your mom lol xd on 2017-01-04.
+ * Lenny's a cuck xd
+ */
+public class main {
+    public static void main (String [] args){
+        System.out.println("LENNY IS A PIECE OF SHIT xd");
+        System.out.println("ayy I'm doing it");
+        System.out.println("are you proud of me?");
+    }
+}
